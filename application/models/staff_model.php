b45d88f84002874ce5b7b0093e9c37665b1bf60a
swaposwapna <swaposwapna@gmail.com>
2018-06-13 16:43:51 -0700

    removed the duplication of hospitals in staff_model

--- application/models/staff_model.php
+++ application/models/staff_model.php
@@ -37,9 +37,11 @@ class Staff_model extends CI_Model{
 	function user_hospital($user_id = false){
 		if(!!$user_id)
 			$this->db->where('user_hospital_link.user_id',$user_id);
-		$this->db->select('hospital.hospital_id,hospital,hospital_short_name,description,place,district,state,logo')->from('user')
+		$this->db->select('hospital.hospital_id,hospital,hospital_short_name,description,place,district,state,logo')
+		->from('user')
 		->join('user_hospital_link','user.user_id=user_hospital_link.user_id')
-		->join('hospital','user_hospital_link.hospital_id=hospital.hospital_id')		
+		->join('hospital','user_hospital_link.hospital_id=hospital.hospital_id')
+		->group_by('hospital')		
 		->order_by('hospital');
 		$query=$this->db->get();
 		return $query->result();
